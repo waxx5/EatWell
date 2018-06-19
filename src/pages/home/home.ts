1645e71eae94144ce033242f04f04f60807fba2f
Luis Demetrio <luis_demetrio_c@hotmail.com>
2018-06-19 16:22:23 -0500

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  tabBarElement: any;
+  splash = true;
+
+  constructor(public navCtrl: NavController) {
+    this.tabBarElement = document.querySelector('.tabbar');
+  }
+
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      this.tabBarElement.style.display = 'flex';
+    }, 4000);
+  }
+
+
+}
